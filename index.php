306cf3ac51c832eb0106704e09fbaa88c91b460b
Delta R F D <deltarfd@live.com>
2018-12-10 15:03:53 +0700

    Edit Condition "Quotes"

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require __DIR__ . '/vendor/autoload.php';
-     
+
     use \LINE\LINEBot;
     use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
     use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
@@ -10,39 +10,44 @@
     use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
     use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
     use \LINE\LINEBot\SignatureValidator as SignatureValidator;
-     
+
     // set false for production
     $pass_signature = true;
-     
+
     // set LINE channel_access_token and channel_secret
     $channel_access_token = "53cEcYcyR80IH2JWEkoGMZv2P6yiQix40ssNJaby/4m9V9UR4QzEhtZvS7qBsOFDOY2KgJrOOZ1IfOMS4NnBgxgpC5yGuP8B+bVAyUmFWQxKJBAsMesdE/iETsbTutM4h34CFQMVGtUKZM39p9xf8AdB04t89/1O/w1cDnyilFU=";
     $channel_secret = "f7abccce8e4841fa2d082a061e1fb61e";
-     
+
     // inisiasi objek bot
     $httpClient = new CurlHTTPClient($channel_access_token);
     $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
-     
+
     $configs =  [
         'settings' => ['displayErrorDetails' => true],
     ];
     $app = new Slim\App($configs);
-     
+
     // buat route untuk url homepage
     $app->get('/', function($req, $res)
     {
       echo "Welcome at Slim Framework";
     });
-     
+
     //Favqs.com API
     $defaults = array(
         CURLOPT_URL             => 'https://favqs.com/api/qotd.json',
         CURLOPT_POST            => false,
-        CURLOPT_HEADER          => 'Authorization: Token token="8cef8b786e4874bc90b480f682e67fab"',
+        CURLOPT_HEADER          => false,
         CURLOPT_SSL_VERIFYPEER  => false,
         CURLOPT_RETURNTRANSFER  => true,
         CURLOPT_SSL_VERIFYHOST  => false,
     );
+    $headers = array {
+      'Content-Type': 'application/json',
+      'Authorization': 'Token token= 8cef8b786e4874bc90b480f682e67fab'
+    }
     $curl               = curl_init();
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt_array($curl, $defaults);
     $curl_response      = curl_exec($curl);
     $json_result       = json_decode($curl_response);
@@ -58,58 +63,58 @@
         // get request body and line signature header
         $body        = file_get_contents('php://input');
         $signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : '';
-     
+
         // log body and signature
         file_put_contents('php://stderr', 'Body: '.$body);
-     
+
         if($pass_signature === false)
         {
             // is LINE_SIGNATURE exists in request header?
             if(empty($signature)){
                 return $response->withStatus(400, 'Signature not set');
             }
-     
+
             // is this request comes from LINE?
             if(! SignatureValidator::validateSignature($body, $channel_secret, $signature)){
                 return $response->withStatus(400, 'Invalid signature');
             }
         }
-     
+
         // kode aplikasi nanti disini
         $data = json_decode($body, true);
         if(is_array($data['events'])){
             foreach ($data['events'] as $event)
             {
                 if ($event['type'] == 'message')
-                {   
+                {
                     if(
                         $event['source']['type'] == 'group' or
                         $event['source']['type'] == 'room'
                       ){
-                       //message from group / room      
+                       //message from group / room
                         if($event['source']['userId']){
                             $userId     = $event['source']['userId'];
                             $getprofile = $bot->getProfile($userId);
                             $profile    = $getprofile->getJSONDecodedBody();
                             $textMessageBuilder = new TextMessageBuilder("Halo, ".$profile['displayName']);
-                            $stickerMessageBuilder = new StickerMessageBuilder(2, 179);   
+                            $stickerMessageBuilder = new StickerMessageBuilder(2, 179);
                             $multiMessageBuilder = new MultiMessageBuilder();
 
                             $multiMessageBuilder->add(textMessageBuilder);
                             $multiMessageBuilder->add(textMessageBuilder);
-                        
+
                             $result = $bot->replyMessage($event['replyToken'], $multiMessageBuilder);
                             return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-                        
-                        } 
+
+                        }
                         else {
                             // send same message as reply to user
                             $result = $bot->replyText($event['replyToken'], $event['message']['text']);
                             return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-                        }        
+                        }
                       } //message from single user
                     else {
-                        if (strtolower($event['message']['text']) == ('quotes' || 'quote'){
+                        if (strtolower($event['message']['text']) == ('quotes' || 'quote')) {
 
                             $result = $bot->replyText($event['replyToken'], $json_result['body']);
 
@@ -137,11 +142,11 @@
                         {
                             // send same message as reply to user
                             $result = $bot->replyText($event['replyToken'], $event['message']['text']);
-             
+
                             // or we can use replyMessage() instead to send reply message
                             // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
                             // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-             
+
                             return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
                         }
                         elseif(
@@ -155,13 +160,13 @@
                             $contentType = ucfirst($event['message']['type']);
                             $result = $bot->replyText($event['replyToken'],
                                 $contentType. " yang Anda kirim bisa diakses dari link:\n " . $contentURL);
-                         
+
                             return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
                         }
-                    } 
-                    
+                    }
+
                 }
-            } 
+            }
         }
         return $response->withStatus(400, 'No event sent!');
     });
@@ -175,7 +180,7 @@
         $multiMessageBuilder->add($textMessageBuilder);
         $multiMessageBuilder->add($stickerMessageBuilder);
         $result = $bot->pushMessage($userId, $multiMessageBuilder);
-       
+
         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
     });
     $app->get('/multicast', function($req, $res) use ($bot)
@@ -187,11 +192,11 @@
             'U001681440b0dbd195aa130a7abff67b4',
             'U001681440b0dbd195aa130a7abff67b4',
             'U001681440b0dbd195aa130a7abff67b4'];
-     
+
         // send multicast message to user
         $textMessageBuilder = new TextMessageBuilder('Halo, ini pesan multicast');
         $result = $bot->multicast($userList, $textMessageBuilder);
-       
+
         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
     });
 
@@ -201,7 +206,7 @@
         $route  = $req->getAttribute('route');
         $userId = $route->getArgument('userId');
         $result = $bot->getProfile($userId);
-                 
+
         return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
     });
 
@@ -211,10 +216,10 @@
         $route      = $req->getAttribute('route');
         $messageId = $route->getArgument('messageId');
         $result = $bot->getMessageContent($messageId);
-     
+
         // set response
         $res->write($result->getRawBody());
-     
+
         return $res->withHeader('Content-Type', $result->getHeader('Content-Type'));
     });
     $app->run();
